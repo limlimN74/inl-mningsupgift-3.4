780e5a4bee0ea06fea9859b608dbf5bff4229bb5
lini07003 <lini07003@E-PW05C37N>
2024-11-13 12:30:26 +0100

    Add project files.

---
+++ inlämningsupgift 3.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace InlämningsUpgift_3._4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange låtens längd i minuter och sekunder: ");
+            Console.Write("Minuter: ");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.Write("Sekunder: ");
+            int sekunder = int.Parse(Console.ReadLine());
+
+            int totSekunder = minuter * 60 + sekunder;
+            int minSekunder = 165;
+            int maxSekunder = 260;
+
+            if (totSekunder >= minSekunder && totSekunder <= maxSekunder)
+            {
+                Console.WriteLine("Låten får spelas på radiostationen.");
+            }
+            else
+            {
+                Console.WriteLine("Låten får INTE spelas på radiostationen.");
+            }
+        }
+    }
+}
